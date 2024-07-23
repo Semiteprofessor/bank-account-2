061c7cf5827a5c2f215045eb650cb1529d5358c3
Taiwo Olapade <semiteprofessor@gmail.com>
2024-07-23 20:57:57 +0100

    Commit

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { createStore } from "redux";
+import { combineReducers, createStore } from "redux";
 
 const initialState = {
   balance: 0,
@@ -6,7 +6,17 @@ const initialState = {
   loanPurpose: "",
 };
 
-const reducer = (state = initialState, action) => {
+const initialStateCustomer = {
+  customerId: "",
+  customerName: "",
+  customerAddress: "",
+  customerPhoneNumber: "",
+  customerEmail: "",
+  customerLoans: [],
+  createdAt: "",
+};
+
+const accountReducer = (state = initialState, action) => {
   switch (action.type) {
     case "account/deposit":
       return {
@@ -38,7 +48,47 @@ const reducer = (state = initialState, action) => {
   }
 };
 
-const store = createStore(reducer);
+const customerReducer = (state = initialStateCustomer, action) => {
+  switch (action.type) {
+    case "customer/create":
+      return {
+        ...state,
+        customerId: action.payload.customerId,
+        customerName: action.payload.customerName,
+        customerAddress: action.payload.customerAddress,
+        customerPhoneNumber: action.payload.customerPhoneNumber,
+        customerEmail: action.payload.customerEmail,
+        customerLoans: [
+          ...state.customerLoans,
+          {
+            loanId: action.payload.loanId,
+            amount: action.payload.amount,
+            purpose: action.payload.purpose,
+          },
+        ],
+        createdAt: action.payload.createdAt,
+      };
+
+    case "customer/update":
+      return {
+        ...state,
+        customerName: action.payload.customerName,
+        customerAddress: action.payload.customerAddress,
+        customerPhoneNumber: action.payload.customerPhoneNumber,
+        customerEmail: action.payload.customerEmail,
+      };
+
+    default:
+      return state;
+  }
+};
+
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
+});
+
+const store = createStore(rootReducer);
 
 store.dispatch({ type: "account/deposit", payload: 500 });
 // store.dispatch({ type: "account/withdraw", payload: 700 });
@@ -70,5 +120,28 @@ store.dispatch(withdraw(800));
 store.dispatch(requestLoan(50000, "Buy a car"));
 store.dispatch(payLoan());
 console.log(store.getState());
+
+const createCustomer = (
+  customerId,
+  customerName,
+  customerAddress,
+  customerPhoneNumber,
+  customerEmail,
+  customerLoans,
+  createdAt
+) => {
+  return {
+    type: "customer/create",
+    payload: {
+      customerId,
+      customerName,
+      customerAddress,
+      customerPhoneNumber,
+      customerEmail,
+      customerLoans,
+      createdAt: new Date().toISOString(),
+    },
+  };
+};
 
 export default store;
